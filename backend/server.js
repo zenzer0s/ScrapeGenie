741ad0ceea61a0b792aad866d77adfad66e7a976
zenzer0s <praveenzalaki200@gmail.com>
2025-02-26 13:42:59 +0000

    update port configuration to 8000 in Dockerfile and server.js

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const scrapeRoutes = require("./routes/scrape");
 
 const app = express();
-const PORT = 5000;
+const PORT = 8000;
 
 app.use(cors());
 app.use(express.json());
